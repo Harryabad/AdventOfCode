feae0ab9ed73a9536d704c2b27e29e58d349d1b7
Harry Abad <harryabad94@gmail.com>
2023-02-14 16:27:38 +0000

    Day 6 part 2 complete

--- Day 6 Tuning Trouble.py
+++ Day 6 Tuning Trouble.py
@@ -1,6 +1,27 @@
 with open("Day 6 puzzle.txt") as f:
     lines = f.read().rstrip('\n').split('\n')
 
+# listy = []
+# newlist = []
+# count = 0
+# for i in lines:
+#     line = i
+    
+# for i in line:
+#     listy.append(i)
+
+# for i in listy:
+#     count +=1
+#     newlist.append(i)
+#     if len(newlist) > 4:
+#         newlist = newlist[1:4]
+#         newlist.append(i)
+
+#         if (len(set(newlist)) == len(newlist)) and count > 4:
+#             print(count)
+#             break
+
+#PART 2
 listy = []
 newlist = []
 count = 0
@@ -14,13 +35,9 @@ for i in listy:
     count +=1
     newlist.append(i)
     if len(newlist) > 4:
-        newlist = newlist[1:4]
+        newlist = newlist[1:14]
         newlist.append(i)
 
         if (len(set(newlist)) == len(newlist)) and count > 4:
             print(count)
             break
-
-        
-# print(newlist)
-# print(x == i)
